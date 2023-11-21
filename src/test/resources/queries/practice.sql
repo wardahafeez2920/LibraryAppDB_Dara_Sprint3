6a6c62b535480fd8ae061c944880b73a91b3eb2b
Zeeatree <zulpikar27@gmail.com>
2023-11-21 15:00:44 -0500

    finished us05

--- src/test/resources/queries/practice.sql
+++ src/test/resources/queries/practice.sql
@@ -9,3 +9,11 @@ FROM books B
 -- US02 -1 -WH
 select count(*) from book_borrow
 where returned_date is null ;
+
+-- US05 -1
+select bc.name, count(*)
+from book_borrow bb
+         inner join books b on bb.book_id = b.id
+         inner join book_categories bc on b.book_category_id = bc.id
+group by name
+order by 2 desc;
