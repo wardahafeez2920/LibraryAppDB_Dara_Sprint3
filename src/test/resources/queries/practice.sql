f55ff7d9f1c94d3e1eeab981f8ba6dd6a67e47ea
jawaddanguir <danguir0921@gmail.com>
2023-11-23 12:44:40 -0500

    us02_Jawad

--- src/test/resources/queries/practice.sql
+++ src/test/resources/queries/practice.sql
@@ -39,3 +39,7 @@ inner join book_borrow on u.id = bb.user.id
 inner join books b on bb.book.id = b.id
 where full_name = 'Test Student 1' and name= 'Head First Java'
 order by 3 desc;
+
+--US02-1-JD
+select count(*) from book_borrow where is_returned=0;
+
