b9ce1f088883c5f5cf74e834c623ec4c4f81b771
alinaaminova <alinaaminova85@gmail.com>
2023-11-24 15:58:59 -0500

    Finished US01

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@kv",
+        tags = "@aa",
         publish = true //generating a report with public link
 )
 public class CukesRunner {
